854c4d9b754bec4d2ef8274b8ca775cb7eb74c11
Philippe BENOIST <shantee@users.noreply.github.com>
2020-02-04 23:49:38 +0100

    atom

--- 1-install-softwares.sh
+++ 1-install-softwares.sh
@@ -4,11 +4,13 @@
 #   DO NOT JUST RUN THIS. EXAMINE AND JUDGE. RUN AT YOUR OWN RISK.
 #
 ##################################################################################################################
+
 sudo -s
 # installation of zippers and unzippers
 apt install -y p7zip-rar p7zip-full unace unrar zip unzip sharutils rar uudeview mpack arj cabextract
 #softwares from 'normal' repositories / essential
-apt install -y apt-transport-https openssl software-properties-common python-software-properties git zlib1g zlib1g-dev libpcre3 libpcre3-dev build-essential libssl-dev httpie ffmpeg
+apt install -y apt-transport-https build-essential libssl-dev openssl software-properties-common python-software-properties zlib1g zlib1g-dev libsecret-1-dev libx11-dev libxkbfile-dev libpcre3 libpcre3-dev
+apt install -y fakeroot rpm git httpie ffmpeg
 # systems tools / misc
 apt install -y gparted inxi htop ksysguard hardinfo searchmonkey deepin-terminal wine
 apt install -y  dconf-cli dconf-editor hardinfo ppa-purge
@@ -38,6 +40,15 @@ chmod +x /usr/bin/ser
 
 # misc softwares
 # Atom
+ wget -O ~/$TMPDIR/atomdeb https://atom.io/download/deb  -q --show-progress --progress=bar:force 2>&1
+ dpkg -i ~/$TMPDIR/atomdeb
+ rm -f ~/$TMPDIR/atomdeb
+ 
+# Boot-repair
+add-apt-repository -y ppa:yannubuntu/boot-repair
+apt-get -y update
+apt-get install -y boot-repair
+
 #sh software/install-atom.sh
 # Charle-proxy
 wget -q -O - https://www.charlesproxy.com/packages/apt/PublicKey | sudo apt-key add -
