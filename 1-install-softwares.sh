d03a627c47159efdaeb96625d6db0ef3f882eb21
Philippe BENOIST <shantee@users.noreply.github.com>
2020-02-04 13:48:54 +0100

    akout  charles proxy

--- 1-install-softwares.sh
+++ 1-install-softwares.sh
@@ -39,6 +39,11 @@ chmod +x /usr/bin/ser
 # misc softwares
 # Atom
 #sh software/install-atom.sh
+# Charle-proxy
+wget -q -O - https://www.charlesproxy.com/packages/apt/PublicKey | sudo apt-key add -
+sh -c 'echo deb https://www.charlesproxy.com/packages/apt/ charles-proxy main > /etc/apt/sources.list.d/charles.list'
+apt update
+apt install charles-proxy
 
 # create softwares folder (logiciels)
 mkdir -p ~/home/$USER/logiciels
