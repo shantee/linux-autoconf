f99ce0a879324a54bab13ac6c1a38df66929b594
Philippe BENOIST <shantee@users.noreply.github.com>
2020-02-05 15:03:50 +0100

    typo

--- autoconf.sh
+++ autoconf.sh
@@ -1,15 +1,14 @@
 #!/bin/bash
-# Created by Philippe BENOIST zfx.fr
-# I use it to suit my own personnal needs
-# edit and check both files removelist.txt and installlist.txt if you need to install / remove other software
-# couleurs : \e[1;49;32m = bold green  \e[1;31m = bold; red \e[1;95m=bold;magenta \e[0m :normal
+## Created by Philippe BENOIST zfx.fr
+## I use it to suit my own personnal needs
+## edit and check both files removelist.txt and installlist.txt if you need to install  remove other software
+## couleurs : \e[1;49;32m = bold green  \e[1;31m = bold; red \e[1;95m=bold;magenta \e[0m :normal
 TMPDIR="$HOME/autoconftmp"
 INSTALLLIST="installlist.txt" # LISTE DES PAQUET A INSTALLER
 REMOVELIST="removelist.txt" # LISTE DES PAQUETS A SUPPRIMER
 SOFTDIR="$HOME/logiciels" # DOSSIER INSTALLATION POUR CERTAINS LOGICIELS
 NUNU='/usr/bin/nunu' # COMMANDE POUR LANCER NUNUSTUDIO
 source $(dirname "$0")/func.sh
-
 
 show_logo
 mkdir -p $TMPDIR
@@ -18,11 +17,10 @@ echo -e "\e[95mYou need to enter your sudo password to install pacakges\e[0m"
 [ "$UID" -eq 0 ] || exec sudo bash "$0" "$@"
 
 while IFS= read -r line || [[ -n "$line" ]]; do
-    if [ $(dpkg-query -W -f='${Status}' $line 2>/dev/null | grep -c "ok installed") -eq 0 ];
+  if [ $(dpkg-query -W -f='${Status}' $line 2>/dev/null | grep -c "ok installed") -eq 0 ];
 	then
-  	  echo -e "\e[1;49;32m -> installation de : \e[1;49;33m $line \e[0m";
-	  echo ""
-#	  sudo apt install -y $line 
+  	  echo -e "\e[1;49;32m -> installing : \e[1;49;33m $line \e[0m";
+	    sudo apt install -y $line 
 	fi
 done < "$INSTALLLIST"
  
@@ -33,12 +31,11 @@ if [[ $REPLY =~ ^[YyOo]$ ]]
 then
  while IFS= read -r line || [[ -n "$line" ]]; do
     if [ $(dpkg-query -W -f='${Status}' $line 2>/dev/null | grep -c "ok installed") -eq 1 ];
-	then
-  	  echo -e "\e[1;95m -> suppression de : \e[1;95;33m $line \e[0m";
-	  echo 
-#	  sudo apt remove -y $line 
-	fi
-done < "$REMOVELIST"
+	  then
+  	  echo -e "\e[1;95m -> removing : \e[1;95;33m $line \e[0m";
+	    sudo apt remove -y $line 
+	  fi
+ done < "$REMOVELIST"
 fi
 echo -e "\e[1;49;32m-> updating APT (update)\e[0m";
 apt update
@@ -74,8 +71,6 @@ echo -e "\e[1;49;32m ___________________________________________________________
 echo -e "\e[1;49;32m          installation de divers logiciels utiles  \e[0m"
 echo -e "\e[1;49;32m              (micro, arduinoIDE, nunuStudio)      \e[0m"
 echo -e "\e[1;49;32m _______________________________________________________________\e[0m"
-
-
 echo -e "\e[1;49;32m certains programmes seront installés dans le dossier $SOFTDIR\e[0m"
 echo -e "\e[1;49;32m Utiliser ce dossier ? \e[0m"
 read -p "oui= o ou y | non = n (dans le doute tapez o) ::" -n 1 -r
