fe2688537b58ecda73c1f97bc3dd198e84e0a1d3
Philippe BENOIST <shantee@users.noreply.github.com>
2020-02-09 18:29:54 +0100

    Update func.sh

--- func.sh
+++ func.sh
@@ -18,8 +18,8 @@ show_logo(){
   echo
 }
 create_ser(){
-  echo -e "\e[1mCreating ser command\e[0m"
-  echo -e "\e[1min /usr/bin/ser\e[0m"
+  echo
+  echo -e "\e[1m======  Creating command : ser in /usr/bin/ser  ======\e[0m"
   FILE="/usr/bin/ser"
   echo '#!/bin/bash'>>$FILE
   echo 'type http-server >/dev/null 2>&1 || { echo >&2 "you need to install http-server (npm install http-server)"; exit 1; }'>>$FILE
@@ -36,18 +36,21 @@ create_ser(){
   chmod +x /usr/bin/ser
 }
 
-#install wifi drivers rtl8821ce (for asustufxxx and a lot of others laptops)
 install_rtl8821(){
-apt-get install --reinstall git dkms build-essential linux-headers-$(uname -r)
-cd ~/"$TMPDIR"
-git clone https://github.com/tomaspinho/rtl8821ce
-cd rtl8821ce
-chmod +x dkms-install.sh
-chmod +x dkms-remove.sh
-sudo ./dkms-install.sh
+  echo 
+  echo -e "\e[1m==========  Installing WIFI DRIVER RTL8821  ==========\e[0m" 
+  apt-get install --reinstall git dkms build-essential linux-headers-$(uname -r)
+  cd "$TMPDIR"||echo -e "\e[101mProblemn changing dir.\e[0m"
+  git clone https://github.com/tomaspinho/rtl8821ce
+  cd rtl8821ce||echo -e "\e[101mError entering dir rtl8821ce.\e[0m"
+  chmod +x dkms-install.sh
+  chmod +x dkms-remove.sh
+  sudo ./dkms-install.sh
 }
-## fonctions installations logiciels ###################################################
+
 install_nunustudio(){
+  echo 
+  echo -e "\e[1m=============  Installing nunuStudio  ===============\e[0m"  
   cd "$SOFTDIR"||echo -e "\e[101mProblemn changing dir.\e[0m"
   git clone https://github.com/tentone/nunuStudio --progress
   echo -e "\e[102m*************************************\e[0m"
@@ -64,32 +67,44 @@ install_nunustudio(){
   echo "cd ~/">>$NUNU
   chmod +x /usr/bin/nunu
 }
+## restore bash profile
+set_bashprofile(){
+ echo 
+ echo -e "\e[1m===========  Restoring bash profile  ==============\e[0m"
+ cp data/.bashrc $HOME/.bashrc
+ cp data/.profile $HOME/.profile
+}
 install_arduino(){
   cd "$SOFTDIR"||echo -e "\e[101mProblemn changing dir.\e[0m"
+  echo 
+  echo -e "\e[1m==============  Installing ArduinoIDE  ==============\e[0m" 
   curl -fSL  https://downloads.arduino.cc/arduino-nightly-linux64.tar.xz |tar xvpJ
 }
 install_charles(){
-  echo -e "\e[1mInstalling Charles-Proxy\e[0m"
+  echo 
+  echo -e "\e[1m==============  Installing Charles-Proxy  ==============\e[0m"
   wget -q -O - https://www.charlesproxy.com/packages/apt/PublicKey | sudo apt-key add -
   sh -c 'echo deb https://www.charlesproxy.com/packages/apt/ charles-proxy main > /etc/apt/sources.list.d/charles.list'
   apt update
   apt install charles-proxy
 }
 install_bootrepair(){
-  echo -e "\e[1mInstalling Boot-Repair\e[0m"
+  echo 	
+  echo -e "\e[1m==============  Installing BOOT-REPAIR  ================\e[0m"
   add-apt-repository -y ppa:yannubuntu/boot-repair
   apt-get -y update
   apt-get install -y boot-repair
 }
 
 install_micro(){
-  echo ""
-  echo -e "\e[1mInstalling micro (terminal text editor)\e[0m"
+  echo 
+  echo -e "\e[1m==============  Installing MICRO  ======================\e[0m"
   wget -O "$TMPDIR"/mic https://getmic.ro && bash "$TMPDIR"/mic
   mv "$TMPDIR"/micro /usr/bin/micro
 }
 install_nvm(){
-  echo -e "\e[1m==============  Installing NVM ==========================\e[0m"
+  echo	
+  echo -e "\e[1m==============  Installing NVM  =========================\e[0m"
   wget -qO- "https://raw.githubusercontent.com/nvm-sh/nvm/master/install.sh" --show-progress | bash
   nvm use node
   chown -R $USER:$(id -gn $USER) $HOME/.config
@@ -114,6 +129,8 @@ userinstall_nunustudio(){
   chmod +x /usr/bin/nunu
 }
 userinstall_arduino(){
+  echo 
+  echo -e "\e[1m==============  Installing ArduinoIDE  ==============\e[0m" 
   cd "$USERSOFTDIR"||echo -e "\e[101mProblemn changing dir.\e[0m"
   curl -fSL  https://downloads.arduino.cc/arduino-nightly-linux64.tar.xz |tar xvpJ
 }
